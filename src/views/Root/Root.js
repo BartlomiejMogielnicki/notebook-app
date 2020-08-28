22711784717a287f79b52c79e19195a9e82003ae
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-08-28 16:56:21 +0200

    Create globalstyles and buttons with styled components

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
-const Root = () => <h1>App</h1>;
+const Root = () => (
+  <div>
+    <GlobalStyle />
+    <h1>Welcome</h1>
+    <Button width="220px">Close/Save</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
 
 export default Root;
